f7859c6a3832847f44d32214b044b728635f437d
micbou <contact@micbou.com>
2017-08-15 19:08:41 +0200

    Add Python 3 to PATH

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -167,15 +167,26 @@ Invoke-Expression "& 7z x '$upx_output' -oC:\" | out-null
 $env:PATH = "C:\upx391w;$env:PATH"
 
 #
-# Download and install pip for Bintray script requirement.
+# Configure Python.
 #
 
+# Download and install pip for Bintray script requirements.
 $pip_installer_name = "get-pip.py"
 $pip_url = "https://bootstrap.pypa.io/get-pip.py"
 $pip_output = "$env:APPVEYOR_BUILD_FOLDER\downloads\$pip_installer_name"
 Invoke-Download $pip_url $pip_output
 Invoke-Expression "& python '$pip_output'"
 Invoke-Expression "& C:\Python27\Scripts\pip install requests twitter"
+
+# Add Python 3 to PATH.
+$python3_version_array = $env:python3_version.Split('.')
+$python3_minimal_version = $python3_version_array[0] + $python3_version_array[1]
+If ($env:arch -eq 32) {
+    $python3_path = "C:\Python$python3_minimal_version"
+} Else {
+    $python3_path = "C:\Python$python3_minimal_version-x64"
+}
+$env:PATH = "$python3_path;$env:PATH"
 
 #
 # Install winpty.
