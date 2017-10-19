171df9f5820b65a29ea342b3dfd329a8cf5fc19f
micbou <contact@micbou.com>
2017-10-19 13:20:28 +0200

    Install the 32-bit and 64-bit versions of GvimExt

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -150,21 +150,39 @@ $env:tcl_path = "C:\ActiveTcl"
 $env:PATH = "$env:tcl_path\bin;$env:PATH"
 
 #
-# Get libintl and libiconv.
+# Get libintl and libiconv for both architectures.
 #
 
-$gettext_installer_name = "gettext0.19.8.1-iconv1.14-shared-$env:arch.exe"
-$gettext_url = "https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.19.8.1-v1.14/$gettext_installer_name"
-$gettext_output = "$env:APPVEYOR_BUILD_FOLDER\downloads\$gettext_installer_name"
-Invoke-Download $gettext_url $gettext_output
-Start-Process "$gettext_output" -ArgumentList "/verysilent /dir=C:\gettext" -Wait
-Copy-Item C:\gettext\bin\libintl-8.dll "$env:APPVEYOR_BUILD_FOLDER\vim\runtime"
-Copy-Item C:\gettext\bin\libiconv-2.dll "$env:APPVEYOR_BUILD_FOLDER\vim\runtime"
-If (Test-Path C:\gettext\bin\libgcc_s_sjlj-1.dll) {
-    Copy-Item C:\gettext\bin\libgcc_s_sjlj-1.dll "$env:APPVEYOR_BUILD_FOLDER\vim\runtime"
+ForEach ($gettext_arch In 32, 64) {
+    $gettext_installer_name = "gettext0.19.8.1-iconv1.14-shared-$gettext_arch.exe"
+    $gettext_url = "https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.19.8.1-v1.14/$gettext_installer_name"
+    $gettext_output = "$env:APPVEYOR_BUILD_FOLDER\downloads\$gettext_installer_name"
+    Invoke-Download $gettext_url $gettext_output
+    $gettext_dir = "C:\gettext$gettext_arch"
+    Start-Process "$gettext_output" -ArgumentList "/verysilent /dir=$gettext_dir" -Wait
+    $runtime_dir = "$env:APPVEYOR_BUILD_FOLDER\vim\runtime"
+    $gvimext_dir = "$runtime_dir\GvimExt$gettext_arch"
+    $gettext_runtime_dir = "$runtime_dir\gettext$gettext_arch"
+    New-Item $gvimext_dir -type directory
+    New-Item $gettext_runtime_dir -type directory
+    Copy-Item $gettext_dir\bin\libintl-8.dll $gvimext_dir
+    Copy-Item $gettext_dir\bin\libintl-8.dll $gettext_runtime_dir
+    Copy-Item $gettext_dir\bin\libiconv-2.dll $gvimext_dir
+    Copy-Item $gettext_dir\bin\libiconv-2.dll $gettext_runtime_dir
+    If (Test-Path $gettext_dir\bin\libgcc_s_sjlj-1.dll) {
+        Copy-Item $gettext_dir\bin\libgcc_s_sjlj-1.dll $gvimext_dir
+        Copy-Item $gettext_dir\bin\libgcc_s_sjlj-1.dll $gettext_runtime_dir
+    }
+}
+$gettext_dir = "C:\gettext$env:arch"
+$runtime_dir = "$env:APPVEYOR_BUILD_FOLDER\vim\runtime"
+Copy-Item $gettext_dir\bin\libintl-8.dll $runtime_dir
+Copy-Item $gettext_dir\bin\libiconv-2.dll $runtime_dir
+If (Test-Path $gettext_dir\bin\libgcc_s_sjlj-1.dll) {
+    Copy-Item $gettext_dir\bin\libgcc_s_sjlj-1.dll $runtime_dir
 }
 
-$env:PATH = "C:\gettext\bin;$env:PATH"
+$env:PATH = "C:\gettext$env:arch\bin;$env:PATH"
 
 #
 # Add NSIS to PATH.
--- appveyor/package.py
+++ appveyor/package.py
@@ -15,8 +15,53 @@ NSIS_DIR = os.path.join(ROOT_DIR, 'nsis')
 DOC_DIR = os.path.join(RUNTIME_DIR, 'doc')
 XXD_DIR = os.path.join(SOURCES_DIR, 'xxd')
 GVIM_EXT_DIR = os.path.join(SOURCES_DIR, 'GvimExt')
+GVIM_EXT32_DIR = os.path.join(SOURCES_DIR, 'GvimExt32')
+GVIM_EXT64_DIR = os.path.join(SOURCES_DIR, 'GvimExt64')
+GVIM_EXT32_RUNTIME_DIR = os.path.join(RUNTIME_DIR, 'GvimExt32')
+GVIM_EXT64_RUNTIME_DIR = os.path.join(RUNTIME_DIR, 'GvimExt64')
 GVIM_NSIS_PATH = os.path.join(NSIS_DIR, 'gvim.nsi')
 GVIM_PACKAGE_PATH = os.path.join(NSIS_DIR, 'gvim-package.exe')
+
+MSVC_BIN_DIR = os.path.join('..', '..', 'VC')
+
+
+def get_msvc_dir(args):
+    if args.msvc == 11:
+        return os.path.join(os.environ['VS110COMNTOOLS'], MSVC_BIN_DIR)
+    if args.msvc == 12:
+        return os.path.join(os.environ['VS120COMNTOOLS'], MSVC_BIN_DIR)
+    if args.msvc == 14:
+        return os.path.join(os.environ['VS140COMNTOOLS'], MSVC_BIN_DIR)
+    if args.msvc == 15:
+        return get_msvc15_dir()
+    raise RuntimeError('msvc parameter should be 11, 12, 14, or 15.')
+
+
+def get_msvc15_dir():
+    vswhere = os.path.join(os.environ['ProgramFiles(x86)'],
+                           'Microsoft Visual Studio',
+                           'Installer',
+                           'vswhere.exe')
+    if not os.path.exists(vswhere):
+        raise RuntimeError('cannot find vswhere. '
+                           'VS 2017 version 15.2 or later is required.')
+
+    installation_path = subprocess.check_output(
+        [vswhere, '-latest', '-property', 'installationPath']
+    ).strip().decode('utf8')
+    return os.path.join(installation_path, 'VC', 'Auxiliary', 'Build')
+
+
+def get_vc_mod(arch):
+    if arch == 64:
+        return 'x86_amd64'
+    return 'x86'
+
+
+def get_nmake_cmd(args, arch):
+    msvc_dir = get_msvc_dir(args)
+    vc_vars_script_path = os.path.join(msvc_dir, 'vcvarsall.bat')
+    return [vc_vars_script_path, get_vc_mod(arch), '&', 'nmake.exe']
 
 
 def generate_uganda_file():
@@ -35,6 +80,14 @@ def generate_uganda_file():
 def rename_package(args):
     os.rename(GVIM_PACKAGE_PATH,
               os.path.join(NSIS_DIR, args.package))
+
+
+def build_gvimext(args, arch):
+    nmake_cmd = get_nmake_cmd(args, arch)
+
+    os.chdir(GVIM_EXT_DIR)
+
+    subprocess.check_call(nmake_cmd + ['clean', 'all'])
 
 
 def generate_package(args):
@@ -64,8 +117,38 @@ def generate_package(args):
     shutil.copy(os.path.join(SOURCES_DIR, 'gvim.exe'),
                 os.path.join(SOURCES_DIR, 'gvim_ole.exe'))
 
+    for directory in [GVIM_EXT32_DIR,
+                      GVIM_EXT64_DIR,
+                      GVIM_EXT32_RUNTIME_DIR,
+                      GVIM_EXT64_RUNTIME_DIR]:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    build_gvimext(args, 32)
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'gvimext.dll'),
+                os.path.join(GVIM_EXT32_DIR, 'gvimext.dll'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'README.txt'),
+                os.path.join(GVIM_EXT32_DIR, 'README.txt'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'gvimext.inf'),
+                os.path.join(GVIM_EXT32_DIR, 'gvimext.inf'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'GvimExt.reg'),
+                os.path.join(GVIM_EXT32_DIR, 'GvimExt.reg'))
+    for filepath in os.listdir(GVIM_EXT32_DIR):
+        shutil.copy(filepath, GVIM_EXT32_RUNTIME_DIR)
+
+    build_gvimext(args, 64)
     shutil.copy(os.path.join(GVIM_EXT_DIR, 'gvimext.dll'),
                 os.path.join(GVIM_EXT_DIR, 'gvimext64.dll'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'gvimext.dll'),
+                os.path.join(GVIM_EXT64_DIR, 'gvimext.dll'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'README.txt'),
+                os.path.join(GVIM_EXT64_DIR, 'README.txt'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'gvimext.inf'),
+                os.path.join(GVIM_EXT64_DIR, 'gvimext.inf'))
+    shutil.copy(os.path.join(GVIM_EXT_DIR, 'GvimExt.reg'),
+                os.path.join(GVIM_EXT64_DIR, 'GvimExt.reg'))
+    for filepath in os.listdir(GVIM_EXT64_DIR):
+        shutil.copy(filepath, GVIM_EXT64_RUNTIME_DIR)
 
     upx = find_executable('upx')
     if upx is None:
@@ -100,6 +183,9 @@ def remove_if_exists(path):
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--msvc', type=int, choices=[11, 12, 14, 15],
+                        default=15, help='choose the Microsoft Visual '
+                        'Studio version (default: %(default)s).')
     parser.add_argument('package', type=str,
                         help='Vim package name.')
 
