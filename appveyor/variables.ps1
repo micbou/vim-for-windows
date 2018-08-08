29e31916d63eaa550fc2a9c0b3a006ad297e6d85
micbou <contact@micbou.com>
2018-08-09 00:00:54 +0200

    Fix Perl and Tcl versions computation

--- appveyor/variables.ps1
+++ appveyor/variables.ps1
@@ -27,8 +27,12 @@ $vim_version = $env:appveyor_repo_tag_name.Substring(1)
 
 If ($env:arch -eq 32) {
     $vim_arch = "x86"
+    $perl_version = $env:perl32_version
+    $tcl_version = $env:tcl32_version
 } Else {
     $vim_arch = "x64"
+    $perl_version = $env:perl64_version
+    $tcl_version = $env:tcl64_version
 }
 
 $msvc = $env:appveyor_build_worker_image.Substring($env:appveyor_build_worker_image.Length-4)
@@ -43,12 +47,12 @@ If ($msvc -eq 2013) {
 $vim_executable_name = "vim$vim_version-$vim_arch.exe"
 
 $lua_major_minor_version = GetMajorMinorVersion $env:lua_version
-$perl_major_minor_version = GetMajorMinorVersion $env:perl_version
+$perl_major_minor_version = GetMajorMinorVersion $perl_version
 $python2_major_minor_version = GetMajorMinorVersion $env:python2_version
 $python3_major_minor_version = GetMajorMinorVersion $env:python3_version
 $racket_major_minor_version = GetMajorMinorVersion $env:racket_version
 $ruby_major_minor_version = GetMajorMinorVersion $env:ruby_version
-$tcl_major_minor_version = GetMajorMinorVersion $env:tcl_version
+$tcl_major_minor_version = GetMajorMinorVersion $tcl_version
 
 $logs = (git show -s --pretty=format:%b $env:appveyor_repo_tag_name) | Out-String
 # AppVeyor uses "\n" as a convention for newlines in GitHub description
